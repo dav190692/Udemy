5af60cf3bfbd1c175a2e322dcf4e3b118ed194f9
dav190692 <dav190692@gmail.com>
2020-09-14 22:20:21 +0400

    14.09.2020-22:20

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-
 "use strict";
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "99");
 
-
 let personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [],
-privat: true,
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: true,
 };
 
-
-for(let i = 1; i < 3; i++){
-    let lastFilm = prompt("Один из последних просмотренных фильмов?");
-    let rateLastFilm = +prompt("На сколько оцените его?");
+for (let i = 1; i < 3; i++) {
+  let lastFilm = prompt("Один из последних просмотренных фильмов?");
+  let rateLastFilm = +prompt("На сколько оцените его?");
+  if (lastFilm === null || lastFilm.length > 50 || lastFilm == "" || isNaN(rateLastFilm)) {
+      console.log("Error");
+    //   console.log();
+    i--;
+  } else {
     personalMovieDB.movies[lastFilm] = rateLastFilm;
+  }
+}
 
-}console.dir(personalMovieDB);
+if(personalMovieDB.count < 10){
+alert("Просмотрено довольно мало фильмов");
+}else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30){
+alert("Вы классический зритель");
+}else if(personalMovieDB.count > 30){
+    alert("Вы киноман");
+}else{
+    alert("Произошла ошибка");
+}
+
+
+console.dir(personalMovieDB);
